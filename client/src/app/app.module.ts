f3ff2f4749ac805d5c2e70b719f55d20656fb5ae
nicolas <nicolas_zanardo@live.fr>
2021-03-28 20:12:45 +0200

    install bootstrap material

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,31 @@
+// Natives module
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Modules
 import { AppRoutingModule } from './app-routing.module';
+import {LayoutModule} from "./shared/layout/layout.module";
+
+// Components
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { TopbarComponent } from './shared/components/topbar/topbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    SignupComponent,
+    SigninComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/shared/layout/layout.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatCardModule} from "@angular/material/card";
+
+
+const MODULE = [
+  FlexLayoutModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatCardModule
+]
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ...MODULE
+  ],
+  exports: [
+    MODULE
+  ],
+  declarations: []
+})
+export class LayoutModule { }
